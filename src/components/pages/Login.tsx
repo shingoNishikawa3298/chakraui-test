b2edd18a2cacfd20efde3bf657c2617a5a97d200
Shin5 <40261949+shingoNishikawa3298@users.noreply.github.com>
2021-05-26 16:23:36 +0900

    Initial commit

---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,43 @@
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+import { ChangeEvent, memo, useState, VFC } from "react";
+import { useAuth } from "../../hooks/useAuth";
+
+import { PrimaryButton } from "../atoms/buttons/PrimaryButton";
+
+export const Login: VFC = memo(() => {
+  const [userId, setUserId] = useState<string>("");
+
+  const { login, loading } = useAuth();
+
+  const onClickLogin = () => {
+    login(userId);
+  };
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input
+            placeholder="ユーザーID"
+            value={userId}
+            onChange={onChangeUserId}
+          />
+          <PrimaryButton
+            disabled={userId === ""}
+            loading={loading}
+            onClick={onClickLogin}
+          >
+            ログイン
+          </PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
+});
---
+++ src/components/pages/UserManagement.tsx
@@ -0,0 +1,58 @@
+import {
+  Center,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem
+} from "@chakra-ui/react";
+import { memo, useCallback, useEffect, VFC } from "react";
+
+import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { useSelectUser } from "../../hooks/useSelectUser";
+import { User } from "../../types/api/user";
+
+export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { onSelectUser, selectedUser } = useSelectUser();
+  console.log(selectedUser);
+
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users, onOpen });
+    },
+    [users, onSelectUser, onOpen]
+  );
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                id={user.id}
+                imageUrl={"https://source.unsplash.com/random"}
+                userName={user.username}
+                fullName={user.name}
+                onClick={onClickUser}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+
+      <UserDetailModal user={selectedUser} onClose={onClose} isOpen={isOpen} />
+    </>
+  );
+});
